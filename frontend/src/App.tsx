475b473d5d752c5053a7980bd9cd0aa1cd8e25b0
Hasan <hzs2003@live.com>
2024-06-18 21:13:31 +0300

    refactored search to use for and added add portfolio button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,14 +12,20 @@ function App() {
     const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
     const [serverError, setServerError] = useState<string>("");
   
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
       setSearch(e.target.value);
       console.log(e);
       // console.log(search);
     }
+
+    const onPortfolioCreate = (e: SyntheticEvent) => {
+      e.preventDefault();
+      console.log(e);
+    }
   
     // mouse event can cause problem, syntheticEvent is generic with type checking
-    const onClick = async (e: SyntheticEvent) => {
+    const onSearchSubmit = async (e: SyntheticEvent) => {
+      e.preventDefault();
       const result = await searchCompanies(search);
       
       if (typeof result === "string") {
@@ -32,9 +38,9 @@ function App() {
 
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
       {serverError && <h1>{serverError}</h1>}
-      <CardList searchResults={searchResult}/>
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
     </div>
   );
 }
