a191048227a10bb56e24285905102852bedf5042
Hasan <hzs2003@live.com>
2024-06-18 20:25:20 +0300

    made the api call and refactored the logic to be in the app.tsx

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,22 +1,12 @@
-import React, { ChangeEvent, useState, SyntheticEvent } from 'react'
+import React, { ChangeEvent, SyntheticEvent } from 'react'
 
-type Props = {}
+interface Props {
+    onClick: (e: SyntheticEvent) => void;
+    search: string | undefined;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-const Search : React.FC<Props> = (props: Props) : JSX.Element => {
-    // to use typescript<string> generics
-  const [search, setSearch] = useState<string>("");
-  
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-    // console.log(search);
-  }
-
-  // mouse event can cause problem, syntheticEvent is generic with type checking
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
-  }
-
+const Search : React.FC<Props> = ({onClick, search, handleChange}: Props) : JSX.Element => {
   return (
     <div>
         <input value={search} onChange={(e) => handleChange(e)}></input>
