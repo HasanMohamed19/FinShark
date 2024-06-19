2eb7860aacbff906c43d8c7d6e1241cc81cc6e29
Hasan <hzs2003@live.com>
2024-06-19 20:06:52 +0300

    added cashflow

---
+++ frontend/src/Components/CashflowStatement/CashflowStatement.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import { useOutletContext } from 'react-router';
+import { CompanyCashFlow } from '../../company';
+import { getCashFlowStatement } from '../../api';
+import Table from '../Table/Table';
+
+interface Props {}
+
+const config = [
+  {
+    label: "Date",
+    render: (company: CompanyCashFlow) => company.date,
+  },
+  {
+    label: "Operating Cashflow",
+    render: (company: CompanyCashFlow) => company.operatingCashFlow,
+  },
+  {
+    label: "Investing Cashflow",
+    render: (company: CompanyCashFlow) =>
+      company.netCashUsedForInvestingActivites,
+  },
+  {
+    label: "Financing Cashflow",
+    render: (company: CompanyCashFlow) =>
+      company.netCashUsedProvidedByFinancingActivities,
+  },
+  {
+    label: "Cash At End of Period",
+    render: (company: CompanyCashFlow) => company.cashAtEndOfPeriod,
+  },
+  {
+    label: "CapEX",
+    render: (company: CompanyCashFlow) => company.capitalExpenditure,
+  },
+  {
+    label: "Issuance Of Stock",
+    render: (company: CompanyCashFlow) => company.commonStockIssued,
+  },
+  {
+    label: "Free Cash Flow",
+    render: (company: CompanyCashFlow) => company.freeCashFlow,
+  },
+];
+
+const CashflowStatement = (props: Props) => {
+
+  const ticker = useOutletContext<string>();
+  const [cashFlowData, setCashFlowData] = useState<CompanyCashFlow[]>();
+
+  useEffect(() => {
+    const getRatios = async () => {
+      const result = await getCashFlowStatement(ticker);
+      setCashFlowData(result!.data);
+    };
+    getRatios();    
+  }, [])
+
+  return cashFlowData ? (
+    <Table config={config} data={cashFlowData} />
+  ) : (
+    <>Loading...</>
+  )
+}
+
+export default CashflowStatement;
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -7,6 +7,7 @@ import ComapnyProfile from "../Components/CompanyProfile/ComapnyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 import DesignPage from "../Pages/DesignPage/DesignPage";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
+import CashflowStatement from "../Components/CashflowStatement/CashflowStatement";
 
 export const router = createBrowserRouter([
     {
@@ -22,6 +23,7 @@ export const router = createBrowserRouter([
                     {path: "company-profile", element: <ComapnyProfile />},
                     {path: "income-statement", element: <IncomeStatement />},
                     {path: "balance-sheet", element: <BalanceSheet />},
+                    {path: "cashflow-statement", element: <CashflowStatement />},
                 ]
             },
         ]
