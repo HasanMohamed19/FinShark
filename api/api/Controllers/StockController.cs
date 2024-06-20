2fdafbedac6e1b7d3c93990501eecfe52ce90ee5
Hasan <hzs2003@live.com>
2024-06-20 16:22:38 +0300

    added getAll and getById endpoints and dto for stock

---
+++ api/api/Controllers/StockController.cs
@@ -0,0 +1,46 @@
+﻿using api.Data;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+	[Route("/api/stock")]
+	[ApiController]
+    // ControllerBase no view Support
+	public class StockController : ControllerBase
+	{
+        //public IActionResult Index()
+        //{
+        //	return View();
+        //}
+
+        private readonly ApplicationDBContext _dbContext;
+
+        public StockController(ApplicationDBContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _dbContext.Stocks.ToList()
+                .Select(s => s.ToStockDto());
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _dbContext.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock.ToStockDto());
+
+        }
+
+    }
+}
---
+++ api/api/Mappers/StockMappers.cs
@@ -0,0 +1,22 @@
+﻿using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+	public static class StockMappers
+	{
+		public static StockDto ToStockDto(this Stock stockModel)
+		{
+			return new StockDto
+			{
+				Id = stockModel.Id,
+				Symbol = stockModel.Symbol,
+				CompanyName = stockModel.CompanyName,
+				Industry = stockModel.Industry,
+				LastDiv = stockModel.LastDiv,
+				MarketCap = stockModel.MarketCap,
+				Purchase = stockModel.Purchase
+			};
+		}
+	}
+}
