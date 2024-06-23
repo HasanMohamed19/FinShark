95b27a4ba31976e58ae5b38f9a0924bd6529db9d
Hasan <hzs2003@live.com>
2024-06-23 20:28:46 +0300

    added validation

--- api/api/Controllers/CommentController.cs
+++ api/api/Controllers/CommentController.cs
@@ -24,15 +24,26 @@ namespace api.Controllers
 		[HttpGet]
 		public async Task<IActionResult> GetAll()
 		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
 			var comments = await _commentRepo.GetAllAsync();
 			var commentDto = comments.Select(c => c.ToCommentDto());
 
 			return Ok(commentDto);
 		}
 
-		[HttpGet("{id}")]
+		[HttpGet("{id:int}")]
 		public async Task<IActionResult> GetById([FromRoute] int id)
 		{
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
 			var comment = await _commentRepo.GetByIdAsync(id);
 			if (comment == null)
 			{
@@ -42,9 +53,15 @@ namespace api.Controllers
 			return Ok(comment.ToCommentDto());
 		}
 
-		[HttpPost("{stockId}")]
+		[HttpPost("{stockId:int}")]
 		public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto)
 		{
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
 			if (!await _stockRepo.StockExists(stockId))
 			{
 				return BadRequest("Stock does not exist");
@@ -55,9 +72,15 @@ namespace api.Controllers
 			return CreatedAtAction(nameof(GetById), new { id = commentModel.Id }, commentModel.ToCommentDto());
 		}
 
-		[HttpPut("{id}")]
+		[HttpPut("{id:int}")]
 		public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
 		{
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
 			var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate());
 
 			if (comment == null)
@@ -67,9 +90,15 @@ namespace api.Controllers
 			return Ok(comment.ToCommentDto());
 		}
 
-		[HttpDelete("{id}")]
+		[HttpDelete("{id:int}")]
 		public async Task<IActionResult> Delete([FromRoute] int id)
 		{
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
 			var comment = await _commentRepo.DeleteAsync(id);
 			if (comment == null)
 			{
