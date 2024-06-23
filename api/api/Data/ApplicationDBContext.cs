b22f9e03373f1340e95c16be33bb94a217ba2886
Hasan <hzs2003@live.com>
2024-06-23 22:15:52 +0300

    added register endpoint

--- api/api/Data/ApplicationDBContext.cs
+++ api/api/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,8 +12,31 @@ namespace api.Data
 
 		}
 
-        public DbSet<Stock> Stocks { get; set; }
-        public DbSet<Comment> Comments { get; set; }
+		public DbSet<Stock> Stocks { get; set; }
+		public DbSet<Comment> Comments { get; set; }
 
-    }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			// seed rodes, we will have admin and user
+			List<IdentityRole> roles = new List<IdentityRole>
+			{
+				new IdentityRole
+				{
+					Name = "Admin",
+					NormalizedName = "ADMIN"
+				},
+				new IdentityRole
+				{
+					Name = "User",
+					NormalizedName = "USER"
+				},
+			};
+
+			// add the roles to the db
+			builder.Entity<IdentityRole>().HasData(roles);
+
+		}
+	}
 }
---
+++ api/api/Migrations/20240623190811_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "13d9053b-93ce-419b-bfa5-1b33678fa644", null, "User", "USER" },
+                    { "a99d813d-b5ed-4063-9d98-58ae3856a506", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "13d9053b-93ce-419b-bfa5-1b33678fa644");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a99d813d-b5ed-4063-9d98-58ae3856a506");
+        }
+    }
+}
