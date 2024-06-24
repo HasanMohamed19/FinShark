898de84af9b0f38492e4dfac4e3f3d6fd39ddd0d
Hasan <hzs2003@live.com>
2024-06-24 15:32:24 +0300

    added Token Service

--- api/api/Controllers/AccountController.cs
+++ api/api/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using api.Data;
 using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
@@ -13,9 +14,11 @@ namespace api.Controllers
 	public class AccountController : ControllerBase
 	{
 		private readonly UserManager<AppUser> _userManager;
-		public AccountController(UserManager<AppUser> userManager)
+		private readonly ITokenService _tokenService;
+		public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
 		{
 			_userManager = userManager;
+			_tokenService = tokenService;
 		}
 
 		[HttpPost("register")]
@@ -41,7 +44,15 @@ namespace api.Controllers
 					var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
 					if (roleResult.Succeeded)
 					{
-						return Ok("User created");
+						return Ok(
+							new NewUserDto
+							{
+								UserName = appUser.UserName,
+								Email = appUser.Email,
+								Token = _tokenService.CreateToken(appUser)
+							}
+						);
+
 					} else
 					{
 						return StatusCode(500, roleResult.Errors);
--- api/api/Program.cs
+++ api/api/Program.cs
@@ -2,6 +2,7 @@ using api.Data;
 using api.Interfaces;
 using api.Models;
 using api.Repository;
+using api.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -62,9 +63,10 @@ builder.Services.AddAuthentication(options =>
 	};
 });
 
-// to use repository pattern
+// to use repository pattern and other DI
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
