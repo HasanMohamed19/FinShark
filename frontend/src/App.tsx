aff6f553dc40c16fafb748fc32744514933bc7c3
Hasan <hzs2003@live.com>
2024-06-25 20:46:24 +0300

    added missing import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { UserProvider } from './Context/useAuth';
 
 
 function App() {
